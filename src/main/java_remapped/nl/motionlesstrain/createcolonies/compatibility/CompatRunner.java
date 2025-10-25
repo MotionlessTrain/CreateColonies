d16f0fc396c22ef130a80f592f7b33c45b1a4603
MotionlessTrain <timothy@familie-de-moor.nl>
2025-10-25 16:28:35 +0200

    Chore: Remove temporarily remapped files

--- src/main/java_remapped/nl/motionlesstrain/createcolonies/compatibility/CompatRunner.java
+++
@@ -1,8 +0,0 @@
-package nl.motionlesstrain.createcolonies.compatibility;
-
-import java.util.function.Supplier;
-
-@FunctionalInterface
-public interface CompatRunner {
-  void run(Supplier<Runnable> function);
-}
--- src/main/java_remapped/nl/motionlesstrain/createcolonies/hooks/HooksInitialiser.java
+++
@@ -1,9 +0,0 @@
-package nl.motionlesstrain.createcolonies.hooks;
-
-import net.neoforged.neoforge.common.NeoForge;
-
-public class HooksInitialiser {
-  public static void registerHooks() {
-    NeoForge.EVENT_BUS.register(InteractionHook.class);
-  }
-}
